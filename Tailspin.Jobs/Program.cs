8b1f652ab4e06595a6914d742bab2febada801d6
dnelly <lmcmurrian@gmail.com>
2015-11-22 22:54:20 -0500

    added the job

---
+++ Tailspin.Jobs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Topshelf;
+using Microsoft.Owin.Host;
+
+namespace Tailspin.Jobs
+{
+    class Program
+    {
+        private const string Endpoint = "http://localhost:12346";
+
+        static void Main(string[] args)
+        {
+            // AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data"));
+
+            HostFactory.Run(x =>
+            {
+                x.Service<Initializer>(s =>
+                {
+                    s.ConstructUsing(name => new Initializer());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetDescription(Properties.Settings.Default.Description);
+                x.SetDisplayName(Properties.Settings.Default.DisplayName);
+                x.SetServiceName(Properties.Settings.Default.ServiceName);
+
+            });
+        }
+
+
+    }
+
+    class Initializer
+    {
+        readonly Timer _timer;
+        public Initializer()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() {
+            We
+            _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+}
---
+++ Tailspin.Jobs/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+using Hangfire.SqlServer;
+
+[assembly: OwinStartup(typeof(Tailspin.Jobs.Startup))]
+
+namespace Tailspin.Jobs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            //app.UseErrorPage();
+            //app.UseWelcomePage("/");
+            app.UseHangfireDashboard("/");
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(
+                "Tailspin.Jobs.Properties.Settings.DefaultConnection",
+                new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate(
+                () => Console.WriteLine("{0} Recurring job completed successfully!", DateTime.Now.ToString()),
+                Cron.Minutely);
+
+        }
+    }
+}
